ea79057aa4bf574266794816e65d939d7683ac40
beckyjn <rebeccajoynielsen@gmail.come>
2019-05-10 14:09:56 +0100

    creates airport class constructor

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Airport {
+    private ArrayList<Plane> hanger;
+    private String airportCode;
+
+    public Airport(String airportCode){
+        this.hanger = new ArrayList<>();
+        this.airportCode = airportCode;
+    }
 }
