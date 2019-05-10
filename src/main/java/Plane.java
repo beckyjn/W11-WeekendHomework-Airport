13539ffe86a640ac5d56d325ecf8bcb199e3f683
beckyjn <rebeccajoynielsen@gmail.come>
2019-05-10 13:24:57 +0100

    tests plane class is created with no passengers

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -3,5 +3,15 @@ import java.util.ArrayList;
 public class Plane {
     private PlaneType planeType;
     private Airline airline;
-    private int passengers;
+    private ArrayList<Passenger> passengers;
+
+    public Plane(PlaneType planeType, Airline airline){
+        this.planeType = planeType;
+        this.airline = airline;
+        this.passengers = new ArrayList<>();
+    }
+
+    public int countPassengers() {
+        return passengers.size();
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.AIRBUSA220, Airline.EASYJET);
+    }
+
+    @Test
+    public void planeStartsWithNoPassengers() {
+        assertEquals(0, plane.countPassengers());
+    }
 }
