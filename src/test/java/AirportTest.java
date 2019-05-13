d4693ca776edc83dfd3b72f4d805dc6ca88eb00d
beckyjn <rebeccajoynielsen@gmail.come>
2019-05-13 09:11:40 +0100

    all tests passing

--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -5,13 +5,23 @@ import static org.junit.Assert.assertEquals;
 
 public class AirportTest {
 
-    Plane plane;
+    Plane plane1;
+    Plane plane2;
+    Plane plane3;
+    Plane plane4;
     Airport airport;
     @Before
     public void before() {
         airport = new Airport("EDI");
-        plane = new Plane(PlaneType.SMOLPLANE, Airline.LOGANAIR);
-        airport.addPlaneToHangar(plane);
+        plane1 = new Plane(PlaneType.SMOLPLANE, Airline.LOGANAIR);
+        plane2 = new Plane(PlaneType.REGULARSIZEDRUDY, Airline.FLYBE);
+        plane3 = new Plane(PlaneType.BIGBOI, Airline.KLM);
+        plane4 = new Plane(PlaneType.MEGAPLANE, Airline.RYANAIR);
+
+        airport.addPlaneToHangar(plane1);
+        airport.addPlaneToHangar(plane2);
+        airport.addPlaneToHangar(plane3);
+        airport.addPlaneToHangar(plane4);
         airport.createFlight(850, "Orkney");
     }
 
@@ -27,7 +37,7 @@ public class AirportTest {
 
     @Test
     public void canCreateFlights() {
-        assertEquals(0, airport.countPlanesInHangar());
+        assertEquals(3, airport.countPlanesInHangar());
         assertEquals(1, airport.countScheduledFlights());
     }
 
@@ -44,4 +54,6 @@ public class AirportTest {
         airport.sellTicket(850);
         assertEquals(2, airport.countFlightPassengers(850));
     }
+
+    
 }
